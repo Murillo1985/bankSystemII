d5596a5fd71a914aa6615e0428aa0c332ef3ff13
Murillo1985 <murillocalves@gmail.com>
2019-11-27 20:46:50 +0000

    add pom.xml

---
+++ src/main/java/agencyProperties/Client.java
@@ -0,0 +1,26 @@
+package agencyProperties;
+
+public class Client {
+    private String name;
+    private String nif;
+    private String profession;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNif() {
+        return nif;
+    }
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+    public String getProfession() {
+        return profession;
+    }
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
